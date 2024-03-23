1d19566b3047153f438213156a51c489b4645123
Pruthviraj Urankar <pruthvirajurankar@yahoo.in>
2024-03-23 16:28:03 -0700

    added initiate connection

--- client.py
+++ client.py
@@ -1,29 +1,81 @@
 import zmq
+import time
+import random
+from collections import deque
 
+import threading
+
+import constants 
+
+
+
+''' 
+    Global variables from tracking connection requests , packet loss and packet loss rate 
+'''
+connected = False
+seq_num = 0
+socket = None
+packet_loss = False
+
+ 
+def connect(ip_address:str, port:str):
+    global connected, socket
+    
+    address = f'tcp://{ip_address}:{port}'
+    
+    if(connected):
+        print("Already connected")
+        return True
+
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)  # REQ socket for requests
+    socket.connect(address)
+    
+    if socket:
+        connected = True
+        print("Connection established")
+        return True
+    
+    else:
+        print("Connection failed")
+        return False
     
     
-def close_connection(socket: zmq.Socket):
+    
+def close():
+    global connected, socket
+    
+    if not connected:
+        print("Not connected")
+        return
+
     socket.send(b"close")
     socket.close()
+    connected = False
+    socket = None
+    
     print("Connection closed")
+    
+
 
 
 def initiate_connection():
+    global socket
+    
     ip_address = input("Enter the server IP address: ")
     port = input("Enter the server port: ")
     
-    context = zmq.Context()
-    socket = context.socket(zmq.REQ)  # REQ socket for requests
-    socket.connect(f'tcp://{ip_address}:{port}') 
+    if not connect(ip_address, port):
+        return False
     
     # Send connection request
-    socket.send(b"network")
+    socket.send(bytes(constants.INITIATE_CONNECTION_REQUEST, 'utf-8'))
     
     # Wait for acknowledgment
     message = socket.recv()
     recieved_msg = message.decode()
     
-    if socket and recieved_msg == "success":
+    if socket and recieved_msg == constants.CONNECTION_SUCCESS:
         print("Connection established")
         return socket
     
@@ -31,52 +83,70 @@ def initiate_connection():
         print("Connection failed")
         return False
     
-    
 
-def send_by_sliding_window(socket, seq_num, window_size):
-    # Send packet (sequence number)
-    socket.send(f"Packet {seq_num}".encode())
-    
-    # Wait for acknowledgment
+def handle_acknowledgment(socket: zmq.Socket, seq_num: int, packet_num: int, queue: list[tuple[int, int]]):
     message = socket.recv()
-    print(f"Received: {message.decode()}")
+    decoded_message = message.decode()
     
+    ack_seq_num = int(decoded_message.split(":")[1])
+    
+    ack_msg = decoded_message.split(":")[0]
+    if ack_msg == constants.ACKNOWLEDGMENT:
+        seq_num = ack_seq_num + 1
+        
+    return seq_num
+
+def send_by_sliding_window(socket: zmq.Socket):    
     # Implement sliding window and packet loss logic here
-    
-    sliding = []
-    for i in range(window_size):
-        sliding.append(seq_num + i)
+    seq_num = 0
+    packet_num = 0
+    val =(0,1)
+    queue:list[tuple[int, int]] = []
+    for i in range(constants.WINDOW_SIZE):
+        queue.append((seq_num, packet_num))
       
     # send initial packets  
-    for data in sliding:
-        socket.send(f"Packet {data}".encode())
+    for (seq_num, packet_num) in queue:
+        socket.send(f"Data: {seq_num}:{packet_num}".encode())
     
     
     while True:
         message = socket.recv()
-        print(f"Received: {message.decode()}")
-        if message.decode() == "ACK":
-            break
-        else:
-            socket.send(f"Packet {data}".encode())
-            print(f"Resending: {data}")
+        decoded_message = message.decode()
         
-    for i in range(window_size):
-        message = socket.recv()
+        ack_seq_num = int(decoded_message.split(":")[1])
+        
+        ack_msg = decoded_message.split(":")[0]
+        if ack_msg == constants.ACKNOWLEDGMENT:
+            seq_num = handle_acknowledgment(socket, seq_num, packet_num, queue)
+        
+        
+        
+        if decoded_message == constants.ACKNOWLEDGMENT:
+            print("Received: ACK")
+
         print(f"Received: {message.decode()}")
-        sliding.pop(0)
-        sliding.append(seq_num + window_size + i)
-        print(sliding)
+        seq_num += 1
+        packet_num += 1
+        if packet_num == constants.TOTAL_PACKETS:
+            break
+        queue.pop(0)
+        queue.append((seq_num, packet_num))
+        socket.send(f"Data: {seq_num}:{packet_num}".encode())
     
     return seq_num + 1
 
 
 def __main__():
-    socket = initiate_connection()
+    global socket
+    
+    initiate_connection()
     
     if not socket:
         return
     
-    
-    close_connection(socket)
+    close()
     # Implement sliding window and packet loss logic here
+    
+
+__main__()
--- server.py
+++ server.py
@@ -1,16 +1,56 @@
 import zmq
 
-context = zmq.Context()
-socket = context.socket(zmq.REP)  # REP socket for reply (acknowledgments)
-socket.bind("tcp://*:5555")
+import constants
 
-while True:
-    # Receive message
-    print("hello")
+socket = None
+connected = False
+
+def accept_connection():
+    global socket, connected
+    
     message = socket.recv()
-    print(message)
+    decoded_message = message.decode()
     
-    # Process message (simulate packet handling, sequence tracking, etc.)
+    if decoded_message == constants.INITIATE_CONNECTION_REQUEST:
+        socket.send(bytes(constants.CONNECTION_SUCCESS, 'utf-8'))
+        connected = True
+        print("Server accepted connection")
+        
+        return True
     
-    # Send acknowledgment
-    socket.send(b"ACK")
+    return False
+        
+    
+
+def __main__():
+    global socket , connected
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)  # REP socket for reply (acknowledgments)
+    socket.bind(f"tcp://*:{constants.PORT_NUMBER}")
+    
+    if not connected:
+        print("Server waiting for connection")
+        while not accept_connection():
+            pass
+    
+    while True:
+        message = socket.recv()
+        decoded_message = message.decode()
+        
+        if decoded_message == "close":
+            socket.send(b"closed")
+            socket.close()
+            connected = False
+            socket = None
+            print("Connection closed")
+            return
+        
+        socket.send(b"ACK")
+
+__main__()
+    
+    
+    
+
+
+
